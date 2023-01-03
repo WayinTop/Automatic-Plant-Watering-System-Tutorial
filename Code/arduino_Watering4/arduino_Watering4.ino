d15b35c06aba1450b3d9f006f0e2c3bea51538b8
SilentStray <orri@live.com>
2023-01-03 13:09:30 -0500

    Update arduino_Watering4.ino
    Original concept is still the same. Code was slightly simplified and added comments so that those that are English speakers learning to code can more easily see what each section does.

--- Code/arduino_Watering4/arduino_Watering4.ino
+++ Code/arduino_Watering4/arduino_Watering4.ino
@@ -1,84 +1,95 @@
-int IN1 = 2;
-int IN2 = 3;
-int IN3 = 4;
-int IN4 = 5;
+// Define pins for the relay module control pins
+const int IN1 = 2; // For Pump 1
+const int IN2 = 3; // For Pump 2
+const int IN3 = 4; // For Pump 3
+const int IN4 = 5; // For Pump 4
 
-int Pin1 = A0; //模拟引脚A0连接传感器输入
-int Pin2 = A1;
-int Pin3 = A2;
-int Pin4 = A3;
+// Define pins for input for each moisture sensor
+const int Pin1 = A0; // For Sensor 1
+const int Pin2 = A1; // For Sensor 2
+const int Pin3 = A2; // For Sensor 3
+const int Pin4 = A3; // For Sensor 4
 
-float value1 = 0;
-float value2 = 0;
-float value3 = 0;
-float value4 = 0;
+// CHANGE ME - Define thresholds for each moisture sensor. This will depend on how moist the soil needs to be for your particular plant. Calibrate EACH sensor.
+float THRESHOLD1 = 750; // For Sensor 1
+float THRESHOLD2 = 750; // For Sensor 2
+float THRESHOLD3 = 750; // For Sensor 2
+float THRESHOLD4 = 750; // For Sensor 2
+
 void setup() {
   Serial.begin(9600);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
   
-  pinMode(Pin1, INPUT);
-  pinMode(Pin2, INPUT);
-  pinMode(Pin3, INPUT);
-  pinMode(Pin4, INPUT);
+  // Set output pins as outputs
+  pinMode(IN1, OUTPUT); // For Pump 1
+  pinMode(IN2, OUTPUT); // For Pump 2
+  pinMode(IN3, OUTPUT); // For Pump 3
+  pinMode(IN4, OUTPUT); // For Pump 4
   
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, HIGH);
+  // Set input pins as inputs
+  pinMode(Pin1, INPUT); // For Sensor 1
+  pinMode(Pin2, INPUT); // For Sensor 2
+  pinMode(Pin3, INPUT); // For Sensor 2
+  pinMode(Pin4, INPUT); // For Sensor 2
+  
+  // Set output pins to high initially. HIGH = OFF ; LOW = ON
+  digitalWrite(IN1, HIGH); // For Pump 1
+  digitalWrite(IN2, HIGH); // For Pump 2
+  digitalWrite(IN3, HIGH); // For Pump 3
+  digitalWrite(IN4, HIGH); // For Pump 4
   delay(500);
 }
+
 void loop() {
-
-  Serial.print("MOISTURE LEVEL:");
-  value1 = analogRead(Pin1);
+  // Read values from moisture sensors
+  float value1 = analogRead(Pin1);
+  float value2 = analogRead(Pin2);
+  float value3 = analogRead(Pin3);
+  float value4 = analogRead(Pin4);
+  
+  // Check value from moisture sensor 1 and control output accordingly
+  Serial.print("MOISTURE LEVEL on pin ");
+  Serial.print(Pin1);
+  Serial.print(": ");
   Serial.println(value1);
-  if(value1>750)
-  {
-      digitalWrite(IN1, LOW);
-  }
-  else
-  {
+  if (value1 > THRESHOLD1) {
+    digitalWrite(IN1, LOW);
+  } else {
     digitalWrite(IN1, HIGH);
-    }
-    
-  Serial.print("MOISTURE LEVEL:");
-  value2 = analogRead(Pin2);
+  }
+  
+  // Check value from moisture sensor 2 and control output accordingly
+  Serial.print("MOISTURE LEVEL on pin ");
+  Serial.print(Pin2);
+  Serial.print(": ");
   Serial.println(value2);
-  if(value2>750)
-  {
-      digitalWrite(IN2, LOW);
-  }
-  else
-  {
+  if (value2 > THRESHOLD2) {
+    digitalWrite(IN2, LOW);
+  } else {
     digitalWrite(IN2, HIGH);
-    }
-
-  Serial.print("MOISTURE LEVEL:");
-  value3 = analogRead(Pin3);
+  }
+  
+  // Check value from moisture sensor 3 and control output accordingly
+  Serial.print("MOISTURE LEVEL on pin ");
+  Serial.print(Pin3);
+  Serial.print(": ");
   Serial.println(value3);
-  if(value3>750)
-  {
-      digitalWrite(IN3, LOW);
-  }
-  else
-  {
+  if (value3 > THRESHOLD3) {
+    digitalWrite(IN3, LOW);
+  } else {
     digitalWrite(IN3, HIGH);
-    }
-
-  Serial.print("MOISTURE LEVEL:");
-  value4 = analogRead(Pin4);
-  Serial.println(value4);
-  if(value4>750)
-  {
-      digitalWrite(IN4, LOW);
   }
-  else
-  {
+  
+  // Check value from moisture sensor 4 and control output accordingly
+  Serial.print("MOISTURE LEVEL on pin ");
+  Serial.print(Pin4);
+  Serial.print(": ");
+  Serial.println(value4);
+  if (value4 > THRESHOLD4) {
+    digitalWrite(IN4, LOW);
+  } else {
     digitalWrite(IN4, HIGH);
-    }
-    Serial.println();
-  delay(1000);
+  }
+  
+  Serial.println();
+  delay(3600000); //Wait 1 hour before checking the sensors again.
 }
